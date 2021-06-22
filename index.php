e78f337998f7a6bfabcc996543bc6c5b614e97c7
Roberto Vargas <jrobertova98@gmail.com>
2021-06-21 20:31:53 -0500

    commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="funcion.js"></script>
+	<title>Manejo de sesiones</title>
+</head>
+
+<body>
+	<?php
+
+	session_start();
+	if (isset($_SESSION["name"])) {
+		echo "<h1 align='center'>Usar Ajax con Jquery en PHP para comprobar la sesión caducada</h1>";
+		echo "<h1 align='center'>" . $_SESSION["name"] . "</h1>";
+		echo "<p align='center'><a href='logout.php'>Logout</a></p>";
+	} else {
+		header('location:login.php');
+	}
+
+
+	?>
+</body>
+
+</html>
